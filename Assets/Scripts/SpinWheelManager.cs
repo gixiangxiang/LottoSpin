0c5c21bd0fde54ba9f3a64bf0964e2bc2a692df8
gixiangxiang <simon88.wang@gmail.com>
2023-04-25 23:09:23 +0800

    建立轉盤比例代碼
    將轉盤代碼移植至專案

---
+++ Assets/Scripts/SpinWheelManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class SpinWheelManager : MonoBehaviour
+{
+    
+  public Image[] pieImages;
+  public float[] values;
+
+  void Start()
+  {
+    SetValue(values);
+  }
+  
+
+  public void SetValue(float[] valuesToSet)
+  {
+    float totalValues = 0;
+    for (int i = 0; i < pieImages.Length; i++)
+    {
+      totalValues += FindPercentage(valuesToSet,i);
+      pieImages[i].fillAmount=totalValues;
+    }
+  }
+
+  float FindPercentage(float[] valuesToSet, int index)
+  {
+    float totalAmount = 0;
+    for (int i = 0; i < valuesToSet.Length; i++)
+    {
+      totalAmount += valuesToSet[i];
+    }
+    return valuesToSet[index]/totalAmount;
+  }
+}
